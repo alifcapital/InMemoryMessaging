660d308122b00e16557c8c186ed1b0fdb7154d08
Mirolim Majidov <mirolimmajidov@outlook.com>
2025-02-21 12:59:40 +0500

    fixed the OpenTelemetry's naming issue

--- src/Instrumentation/InstrumentationBuilderExtensions.cs
+++ src/Instrumentation/InstrumentationBuilderExtensions.cs
@@ -6,11 +6,11 @@ namespace InMemoryMessaging.Instrumentation;
 public static class InstrumentationBuilderExtensions
 {
     /// <summary>
-    /// Enables the incoming and outgoing events automatic data collection for ASP.NET Core.
+    /// Enables OpenTelemetry instrumentation for in-memory messaging. 
     /// </summary>
     /// <param name="builder"><see cref="T:OpenTelemetry.Trace.TracerProviderBuilder" /> being configured.</param>
     /// <returns>The instance of <see cref="T:OpenTelemetry.Trace.TracerProviderBuilder" /> to chain the calls.</returns>
-    public static TracerProviderBuilder AddEventBusInstrumentation(this TracerProviderBuilder builder)
+    public static TracerProviderBuilder AddMemoryMessagingInstrumentation(this TracerProviderBuilder builder)
     {
         builder.AddSource(InMemoryMessagingTraceInstrumentation.InstrumentationName);
         
