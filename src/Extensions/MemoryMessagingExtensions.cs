0454e27c18d59d14c32c5ec6d798c2b5a011d922
Mirolim Majidov <mirolimmajidov@outlook.com>
2025-02-27 10:31:44 +0500

    Added filtering functionality

--- src/Extensions/MemoryMessagingExtensions.cs
+++ src/Extensions/MemoryMessagingExtensions.cs
@@ -13,29 +13,31 @@ public static class MemoryMessagingExtensions
     /// </summary>
     /// <param name="services">BackgroundServices of DI</param>
     /// <param name="assemblies">Assemblies to find and load all messages including handlers</param>
+    /// <param name="baseMassageTypeToFilter">The base type of the message to filter the message handlers types. The default value is <see cref="IMessage"/>.</param>
     /// <param name="executingReceivedMessage">Events for subscribing to the executing received message</param>
     public static void AddInMemoryMessaging(this IServiceCollection services,
         Assembly[] assemblies,
+        Type baseMassageTypeToFilter = null,
         EventHandler<ReceivedMessageArgs> executingReceivedMessage = null)
     {
         services.AddScoped<IMessageManager, MessageManager>();
 
-        RegisterAllMessageHandlersToDependencyInjectionAndMessagingManager(services, assemblies);
-        
+        RegisterAllMessageHandlersToDependencyInjectionAndMessagingManager(services, assemblies, baseMassageTypeToFilter);
+
         if (executingReceivedMessage is not null)
             MessageManager.ExecutingMessageHandlers += executingReceivedMessage;
     }
-    
+
     #region Message Handlers Registration
 
     internal static void RegisterAllMessageHandlersToDependencyInjectionAndMessagingManager(IServiceCollection services,
-        Assembly[] assemblies)
+        Assembly[] assemblies, Type baseMassageTypeToFilter = null)
     {
         var allMessagesIncludingHandlers = GetAllMessageTypesIncludingHandlers(assemblies);
 
         RegisterAllSubscriberReceiversToDependencyInjection();
         RegisterAllSubscriberReceiversToMemoryMessagingManager();
-        
+
         return;
 
         void RegisterAllSubscriberReceiversToDependencyInjection()
@@ -55,18 +57,19 @@ public static class MemoryMessagingExtensions
     }
 
     private static readonly Type MessageHandlerType = typeof(IMessageHandler<>);
-    private static readonly Type IMassageType = typeof(IMessage);
 
     /// <summary>
     /// Get all message types from the assemblies including the message handler types.
     /// </summary>
     /// <param name="assemblies">The assemblies to find all message handlers</param>
+    /// <param name="baseMassageTypeToFilter">The base type of the message to filter the message handlers types. The default value is <see cref="IMessage"/>.</param>
     /// <returns>All message types including the message handler type</returns>
-    internal static Dictionary<Type, List<Type>> GetAllMessageTypesIncludingHandlers(Assembly[] assemblies)
+    internal static Dictionary<Type, List<Type>> GetAllMessageTypesIncludingHandlers(Assembly[] assemblies, Type baseMassageTypeToFilter = null)
     {
+        var baseMassageType = baseMassageTypeToFilter ?? typeof(IMessage);
         Dictionary<Type, List<Type>> massageHandlerTypes = [];
         if (assemblies is null) return massageHandlerTypes;
-        
+
         var allTypes = assemblies
             .SelectMany(a => a.GetTypes())
             .Where(t => t is { IsClass: true, IsAbstract: false });
@@ -78,21 +81,21 @@ public static class MemoryMessagingExtensions
                     implementedInterface.GetGenericTypeDefinition() == MessageHandlerType)
                 {
                     var eventType = implementedInterface.GetGenericArguments().Single();
-                    if (IMassageType.IsAssignableFrom(eventType))
+                    if (baseMassageType.IsAssignableFrom(eventType))
                         AddMessageHandlerType(eventType, type);
                 }
             }
         }
 
         return massageHandlerTypes;
-        
+
         void AddMessageHandlerType(Type eventType, Type handlerType)
         {
             if (massageHandlerTypes.TryGetValue(eventType, out var handlerTypes))
             {
-                if(handlerTypes.Contains(handlerType))
+                if (handlerTypes.Contains(handlerType))
                     return;
-                
+
                 handlerTypes.Add(handlerType);
             }
             else
--- tests/InMemoryMessaging.Tests/UnitTests/MemoryMessagingExtensionsTests.cs
+++ tests/InMemoryMessaging.Tests/UnitTests/MemoryMessagingExtensionsTests.cs
@@ -9,22 +9,48 @@ public class MemoryMessagingExtensionsTests : BaseTestEntity
     #region GetSubscriberTypes
 
     [Test]
-    public void GetSubscriberTypes_GettingJustCreatedOnThisProjectEvent_ShouldReturnOneExpectedTypeAndHandlerType()
+    public void GetSubscriberTypes_GettingAllMessageTypesByDefaultBaseEventType_ShouldReturnOneTypeWithTwoHandlerTypes()
     {
-        var messageType = typeof(UserCreated);
-        var messageHandlerType1 = typeof(Domain.Module1.UserCreatedHandler);
-        var messageHandlerType2 = typeof(Domain.Module2.UserCreatedHandler);
+        var userCreatedMessageType = typeof(UserCreated);
+        var userCreatedMessageHandlerType1 = typeof(Domain.Module1.UserCreatedHandler);
+        var userCreatedMessageHandlerType2 = typeof(Domain.Module2.UserCreatedHandler);
+        
+        var userUpdatedMessageType = typeof(UserUpdated);
+        var userUpdatedMessageHandlerType = typeof(UserUpdatedHandler);
 
         var handlersInfo = MemoryMessagingExtensions.GetAllMessageTypesIncludingHandlers(
             [typeof(MemoryMessagingExtensionsTests).Assembly]
         );
 
+        Assert.That(handlersInfo.ContainsKey(userCreatedMessageType), Is.True);
+        Assert.That(handlersInfo.ContainsKey(userUpdatedMessageType), Is.True);
+        
+        var userCreatedHandlerTypes = handlersInfo[userCreatedMessageType];
+        Assert.That(userCreatedHandlerTypes, Has.Count.EqualTo(2));
+        Assert.That(userCreatedHandlerTypes, Does.Contain(userCreatedMessageHandlerType1));
+        Assert.That(userCreatedHandlerTypes, Does.Contain(userCreatedMessageHandlerType2));
+
+        var userUpdatedHandlerTypes = handlersInfo[userUpdatedMessageType];
+        Assert.That(userUpdatedHandlerTypes, Has.Count.EqualTo(1));
+        Assert.That(userUpdatedHandlerTypes, Does.Contain(userUpdatedMessageHandlerType));
+    }
+
+    [Test]
+    public void GetSubscriberTypes_GettingAllMessageTypesByCustomEventType_ShouldReturnOneTypeWithOneHandlerType()
+    {
+        var messageType = typeof(UserUpdated);
+        var messageHandlerType = typeof(UserUpdatedHandler);
+
+        var handlersInfo = MemoryMessagingExtensions.GetAllMessageTypesIncludingHandlers(
+            assemblies: [typeof(MemoryMessagingExtensionsTests).Assembly],
+            baseMassageTypeToFilter: typeof(IDomainEvent)
+        );
+
         Assert.That(handlersInfo.ContainsKey(messageType), Is.True);
 
         var handlerTypes = handlersInfo[messageType];
-        Assert.That(handlerTypes, Has.Count.EqualTo(2));
-        Assert.That(handlerTypes, Does.Contain(messageHandlerType1));
-        Assert.That(handlerTypes, Does.Contain(messageHandlerType2));
+        Assert.That(handlerTypes, Has.Count.EqualTo(1));
+        Assert.That(handlerTypes, Does.Contain(messageHandlerType));
     }
 
     #endregion
