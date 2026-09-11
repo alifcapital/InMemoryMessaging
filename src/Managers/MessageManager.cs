eb86ac67cff09091f622a354af5142bd3e83c4f1
Mirolim Majidov <mirolimmajidov@outlook.com>
2026-09-11 15:55:07 +0500

    added cloning object

--- src/Managers/MessageManager.cs
+++ src/Managers/MessageManager.cs
@@ -30,7 +30,7 @@ internal class MessageManager(IServiceProvider serviceProvider) : IMessageManage
     /// <param name="typesOfHandler">The types of the handler.</param>
     internal static void AddHandlers(Type typeOfMessage, Type[] typesOfHandler)
     {
-       const string handleMethodName = nameof(IMessageHandler<IMessage>.HandleAsync);
+       const string handleMethodName = nameof(IMessageHandler<>.HandleAsync);
        
        var handlersWithMethod = typesOfHandler.Select(handlerType =>
         {
@@ -76,6 +76,12 @@ internal class MessageManager(IServiceProvider serviceProvider) : IMessageManage
                     // Because even the message type name is the same, instead of message type can be different, that is may while handling it may throw an exception.
                 }
                 
+                // If the type of publishing and handling message type is not equal, we need to create instead of handling event
+                // and copy its property values from the main publishing message. Otherwise, the message handler may not accept passing the message. 
+                var messageToPublish = messageType == publishingMessageType
+                    ? message
+                    : CloneMessageFromOriginal(messageType, message);
+
                 foreach (var handlerInfo in messageHandlers)
                 {
                     var eventReceiver = serviceProvider.GetRequiredService(handlerInfo.MessageHandlerType);
@@ -91,7 +97,35 @@ internal class MessageManager(IServiceProvider serviceProvider) : IMessageManage
     }
 
     #region Helper methods
-    
+
+    /// <summary>
+    /// Creates an instance of the <paramref name="targetMessageType"/> and copies the matching properties (same name and an assignable type) from the <paramref name="sourceMessage"/>.
+    /// Even though the message type name is the same across modules, the actual message type can be different (e.g. each module may declare its own version of the same event),
+    /// so the original message instance cannot simply be passed to a handler that expects a different type.
+    /// </summary>
+    /// <param name="targetMessageType">The message type expected by the handler.</param>
+    /// <param name="sourceMessage">The message instance that has been published.</param>
+    private static object CloneMessageFromOriginal(Type targetMessageType, IMessage sourceMessage)
+    {
+        var targetMessage = Activator.CreateInstance(targetMessageType);
+        var sourceProperties = sourceMessage.GetType().GetProperties();
+
+        foreach (var targetProperty in targetMessageType.GetProperties())
+        {
+            if (!targetProperty.CanWrite)
+                continue;
+
+            var sourceProperty = sourceProperties.FirstOrDefault(property =>
+                property.Name == targetProperty.Name && property.CanRead && targetProperty.PropertyType.IsAssignableFrom(property.PropertyType));
+            if (sourceProperty is null)
+                continue;
+
+            targetProperty.SetValue(targetMessage, sourceProperty.GetValue(sourceMessage));
+        }
+
+        return targetMessage;
+    }
+
     /// <summary>
     /// Invokes the ExecutingMessageHandlers event to be able to execute another an action before the handler.
     /// </summary>
